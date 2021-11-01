04998217c88b03a3616214b25db53f0e6364f5b9
Manon Schnetzler <bonsoir@mailo.com>
2021-11-01 18:01:50 +0100

    generator sentence ok

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,6 +3,7 @@ require "json"
 class IdeasController < ApplicationController
   def show
     @idea = Idea.find(params[:id])
+    @ideas = Idea.all.order(created_at: :desc).limit(10)
   end
 
   def create
@@ -19,11 +20,22 @@ class IdeasController < ApplicationController
 
   def generate_name_idea
     filepath = 'db/dictionnaire.json'
-
     serialized_dictionary = File.read(filepath)
-
     dictionary = JSON.parse(serialized_dictionary)
     # TODO => keys to_sym
-    raise
+    subject = dictionary['dictionnaire']['sujets'].sample
+    ## SIMPLE SENTENCE
+    # verb = dictionary['dictionnaire']['verbes']['intransitifs'].sample
+    # sentence = "#{subject.capitalize} pour #{verb}."
+    ## SIMPLE SENTENCE +
+    verb = dictionary["dictionnaire"]["verbes"]["transitifs"].sample
+    complement = dictionary["dictionnaire"]["complements"]["objDir"].sample
+    sentence = "#{subject.capitalize} pour #{verb} #{complement}."
+
+    sentence.gsub!(/#\d+/, '')
+    sentence.gsub!(/{/, ' ')
+    sentence.gsub!(/}/, '')
+    sentence
+
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @ideas = Idea.all
+    @ideas = Idea.all.order(created_at: :desc).limit(10)
   end
 end
